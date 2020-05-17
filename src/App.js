6c157e50934e75e4255a7af238b02a577d2d3c59
kharumax <kharumax@gmail.com>
2020-05-17 13:07:34 +0900

    Fin Frontend App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import MovieList from "./components/movie-list";
+import MovieDetails from "./components/movie-details";
+import MovieForm from "./components/movie-form";
+import Login from "./components/login";
+import {withCookies } from "react-cookie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var FontAwesome = require("react-fontawesome");
+
+class App extends Component{
+
+    state = {
+        movies: [],
+        selectedMovie: null,
+        editMovie: null,
+        token: this.props.cookies.get("mr-token")
+    };
+
+    componentDidMount() {
+        if (this.state.token) {
+            //fetch data
+            fetch(`${process.env.REACT_APP_API_URL}/api/movies/`,{
+                method: "GET",
+                headers: {
+                    "Authorization": `Token ${this.state.token}`
+                }
+            })
+                .then(resp => resp.json())
+                .then(resp => this.setState({movies:resp}))
+                .catch(error => console.log(error))
+        } else {
+            window.location.href = "/";
+        }
+
+    }
+
+    loadMovie = movie => {
+        this.setState({
+            selectedMovie: movie,
+            editMovie:null
+        });
+    };
+
+    movieDeleted = selMovie => {
+        const movies = this.state.movies.filter( movie => movie.id !== selMovie.id );
+        this.setState({
+            movies:movies,selectedMovie: null
+        })
+    };
+
+    editClicked = selMovie => {
+        this.setState({
+            editMovie: selMovie
+        });
+    };
+
+    newMovie = () => {
+        this.setState({editMovie:{title:"",description:""}})
+    };
+
+    cancelForm = () => {
+        this.setState({editMovie:null});
+    };
+
+    addMovie = movie => {
+        this.setState({movies: [...this.state.movies,movie]})
+    };
+
+    render() {
+          return (
+              <div className="App">
+                  <h1>
+                      <FontAwesome name="film"/>
+                      <span>Movie Rater</span>
+                  </h1>
+                  <div className="layout">
+                      <MovieList movies={this.state.movies} movieClicked={this.loadMovie}
+                                movieDeleted={this.movieDeleted} editClicked={this.editClicked}
+                                 newMovie={this.newMovie} token = {this.state.token}
+                      />
+                      <div>
+                          { !this.state.editMovie? (
+                              <MovieDetails movie={this.state.selectedMovie} updateMovie={this.loadMovie}
+                                            token = {this.state.token}
+                              />
+                          ) :(
+                              <MovieForm movie={this.state.editMovie} cancelForm={this.cancelForm}
+                                         newMovie={this.addMovie} editedMovie={this.loadMovie}
+                                         token = {this.state.token}
+                              />)}
+                      </div>
+                  </div>
+              </div>
+          );
+      }
 }
 
-export default App;
+export default withCookies(App);
